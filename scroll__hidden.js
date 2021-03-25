b2ca26958649deba67d388fd3b150b04d738e220
sungjoong-kim <80944300+sungjoong-kim@users.noreply.github.com>
2021-03-25 16:27:30 +0900

    Update scroll__hidden.js

--- scroll__hidden.js
+++ scroll__hidden.js
@@ -9,3 +9,18 @@ function handleClickMenu() {
          handleClickMenu();
      })
  })
+
+document.documentElement.addEventListener('touchstart', function (event) {
+     if (event.touches.length > 1) {
+          event.preventDefault(); 
+        } 
+    }, false);
+
+var lastTouchEnd = 0; 
+
+document.documentElement.addEventListener('touchend', function (event) {
+     var now = (new Date()).getTime();
+     if (now - lastTouchEnd <= 300) {
+          event.preventDefault(); 
+        } lastTouchEnd = now; 
+    }, false);
