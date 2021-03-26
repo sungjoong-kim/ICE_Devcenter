2d060bbb9b349aae75ba71334f602023f5cd78a1
sungjoong-kim <80944300+sungjoong-kim@users.noreply.github.com>
2021-03-26 18:07:35 +0900

    Add files via upload

--- scroll__hidden.js
+++ scroll__hidden.js
@@ -10,17 +10,17 @@ function handleClickMenu() {
      })
  })
 
-document.documentElement.addEventListener('touchstart', function (event) {
-     if (event.touches.length > 1) {
-          event.preventDefault(); 
-        } 
-    }, false);
+ document.documentElement.addEventListener('touchstart', function (event) {
+    if (event.touches.length > 1) {
+         event.preventDefault(); 
+       } 
+   }, false);
 
 var lastTouchEnd = 0; 
 
 document.documentElement.addEventListener('touchend', function (event) {
-     var now = (new Date()).getTime();
-     if (now - lastTouchEnd <= 300) {
-          event.preventDefault(); 
-        } lastTouchEnd = now; 
-    }, false);
+    var now = (new Date()).getTime();
+    if (now - lastTouchEnd <= 300) {
+         event.preventDefault(); 
+       } lastTouchEnd = now; 
+   }, false);
