6f3595c00e3ad5f7f9b5b8f1967f10ff1bf8deb9
sungjoong-kim <80944300+sungjoong-kim@users.noreply.github.com>
2021-04-13 10:35:12 +0900

    Add files via upload

--- js/scroll__hidden.js
+++ js/scroll__hidden.js
@@ -26,3 +26,24 @@ document.documentElement.addEventListener('touchend', function (event) {
    }, false);
    
 
+        // 엔터키 클릭 모션
+        $(".login__id").keyup(function (e) { if (e.keyCode == 13) login(); });
+
+        // 모달창 클릭
+    $("#emailreject").on('click', function () {
+        $('.modal__background').show();
+    });
+        // 모달창 닫기
+    $("#emailreject__close").on('click', function () {
+        $('.modal__background').hide();
+    });
+            // 모달창 클릭 스크롤막기
+            $("#emailreject").on('click', function () {
+        $('body').addClass('scroll__hidden');
+    });
+
+        // 모달창 닫기 스크롤막기 풀기
+    $("#emailreject__close").on('click', function () {
+        $('body').removeClass('scroll__hidden');
+    });
+
